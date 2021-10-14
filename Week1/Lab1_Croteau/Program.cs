57f8460b2fac00ba63cf0f733a50f15d2402441f
Croteau <rrcroteau@email.neit.edu>
2021-10-14 11:08:00 -0400

    Week2 Work

--- Week1/Lab1_Croteau/Program.cs
+++ Week1/Lab1_Croteau/Program.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿//Ron Croteau
+//SE245, Fall 2021
+//Lab 1
+/*
+ * Program description: This program takes the name of a student and then the four grades the student earned. 
+ * It finds the average of the four grades and then assigns and outputs a letter grade based on the average.
+ * For extra credit(bonus), the program also uses a weighted grade system based on 20%, 20%, 25%, 35% in order to determine the weighted average and letter grade and outputs the information to the user.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,26 +21,27 @@ namespace Lab1_Croteau
         {
             //declare/initialize variables
             String strName, strGrade1, strGrade2, strGrade3, strGrade4;
-            Double dblGrade1, dblGrade2, dblGrade3, dblGrade4, dblAvg = 0;
+            Double dblGrade1, dblGrade2, dblGrade3, dblGrade4, dblAvg, dblWeightedGrade1, dblWeightedGrade2, dblWeightedGrade3, dblWeightedGrade4, dblWeightedAvg;
 
             //print to the console
             Console.WriteLine("Hello There!");
 
             //print to the console w/o new line
-            Console.Write("Please the student's name: ");
+            Console.Write("Please enter the student's name: ");
             //store user input to variable
             strName = Console.ReadLine();
 
-            Console.WriteLine($"Please enter the first grade for {strName}: ");
+            //solicit the four grades to be used in determining the average
+            Console.Write($"Please enter the first grade for {strName}: ");
             strGrade1 = Console.ReadLine();
 
-            Console.WriteLine($"Please enter the second grade for {strName}: ");
+            Console.Write($"Please enter the second grade for {strName}: ");
             strGrade2 = Console.ReadLine();
 
-            Console.WriteLine($"Please enter the third grade for {strName}: ");
+            Console.Write($"Please enter the third grade for {strName}: ");
             strGrade3 = Console.ReadLine();
 
-            Console.WriteLine($"Please enter the fourth grade for {strName}: ");
+            Console.Write($"Please enter the fourth grade for {strName}: ");
             strGrade4 = Console.ReadLine();
 
             //convert the string numbers to doubles to be used in math operations
@@ -41,8 +51,18 @@ namespace Lab1_Croteau
             dblGrade4 = Double.Parse(strGrade4);
 
             //determine the avg given the four grades entered
-            dblAvg = (dblGrade1 + dblGrade2 + dblGrade3 + dblGrade4) / 4; 
+            dblAvg = (dblGrade1 + dblGrade2 + dblGrade3 + dblGrade4) / 4;
 
+            //for extra credit, get the weighted grades by applying a weighted percentage for the four scores (20%, 20%, 25%, 35%, respectively)
+            dblWeightedGrade1 = dblGrade1 * .2;
+            dblWeightedGrade2 = dblGrade2 * .2;
+            dblWeightedGrade3 = dblGrade3 * .25;
+            dblWeightedGrade4 = dblGrade4 * .35;
+
+            //considering these are weighted grades, you simply need to add them to get the weighted average (as the percentages add up to 100%)
+            dblWeightedAvg = dblWeightedGrade1 + dblWeightedGrade2 + dblWeightedGrade3 + dblWeightedGrade4;
+
+            //figure out the letter grade based on raw scores/average and output to user
             if (dblAvg >= 90)
             {
                 Console.WriteLine($"\n\n{strName} has an average of {dblAvg} for a letter grade of A.");
@@ -66,6 +86,32 @@ namespace Lab1_Croteau
             else
             {
                 Console.WriteLine($"\n\n{strName} has an average of {dblAvg} for a letter grade of F.");
+            }
+
+            //for extra credit, figure out the weighted letter grade based on weighted scores/average and output to user
+            if (dblWeightedAvg >= 90)
+            {
+                Console.WriteLine($"\n\n{strName} has a weighted average of {dblWeightedAvg} for a weighted letter grade of A.");
+            }
+
+            else if (dblWeightedAvg >= 80)
+            {
+                Console.WriteLine($"\n\n{strName} has a weighted average of {dblWeightedAvg} for a weighted letter grade of B.");
+            }
+
+            else if (dblWeightedAvg >= 70)
+            {
+                Console.WriteLine($"\n\n{strName} has a weighted average of {dblWeightedAvg} for a weighted letter grade of C.");
+            }
+
+            else if (dblWeightedAvg >= 60)
+            {
+                Console.WriteLine($"\n\n{strName} has a weighted average of {dblWeightedAvg} for a weighted letter grade of D.");
+            }
+
+            else
+            {
+                Console.WriteLine($"\n\n{strName} has a weighted average of {dblWeightedAvg} for a weighted letter grade of F.");
             }
 
             //Pause the console so that the user can view the information prior to the application closing
---
+++ Week2/Lab2_Croteau/Program.cs
@@ -0,0 +1,164 @@
+﻿//Ron Croteau
+//SE245, Fall 2021
+//Lab 2
+/*
+ * Program description: This program gets a students name and grades for 5 labs. It can take data from more than 1 student.
+ * It displays each student with each average grade for their 5 labs, and their letter grade.  
+ * It then displays the average for each lab #.  (Average grade on Lab #1 for all students)
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab2_Croteau
+{
+    class Program
+    {
+
+        static void GetGrade(List<double> temp, int labNum)
+        {
+            ///This function takes a list and int representing which grade you want to update. It prompts user for the grade for a specific lab, validates the grade, then adds it to the passed list ///
+            
+            //declare/initialize variables
+            String strNum;
+            Double dblNum;
+            bool blnResult = false;
+
+            //create do/while to validate lab grades
+            do
+            {
+                //ask user for lab grade
+                Console.Write($"Please enter the student's grade for Lab {labNum}: ");
+                //store user input for validation
+                strNum = Console.ReadLine();
+
+                //validate if it is a proper double
+                blnResult = Double.TryParse(strNum, out dblNum);
+
+                if (blnResult == false || dblNum < 0)
+                {
+                    Console.WriteLine("\n\nInvalid Input: Please enter a positive real number in digits: ex 92.5");
+                }
+
+            } while (blnResult == false);
+
+            //add the validated lab grade to the appropriate list
+            temp.Add(dblNum);
+        }
+
+        static void Main(string[] args)
+        {
+            //declare/initialize variables
+            String strAnswer;
+            Double dblAvg, dblLab1Total = 0, dblLab2Total = 0, dblLab3Total = 0, dblLab4Total = 0, dblLab5Total = 0;
+            
+            //create lists to store data
+            List<string> fName = new List<string>();
+            List<string> lName = new List<string>();
+            List<string> ltrGrade = new List<string>();
+            List<double> lab1 = new List<double>();
+            List<double> lab2 = new List<double>();
+            List<double> lab3 = new List<double>();
+            List<double> lab4 = new List<double>();
+            List<double> lab5 = new List<double>();
+            List<double> studentAvg = new List<double>();
+
+            //create loop to solicit data from user
+            do
+            {
+                //ask user for first name
+                Console.Write("Please enter the student's first name: ");
+                //store user input to appropriate list
+                fName.Add(Console.ReadLine());
+
+                //ask user for last name
+                Console.Write("Please enter the student's last name: ");
+                //store user input to appropriate list
+                lName.Add(Console.ReadLine());
+
+                //get grades for all 5 labs using the GetGrade function
+                GetGrade(lab1, 1);
+                GetGrade(lab2, 2);
+                GetGrade(lab3, 3);
+                GetGrade(lab4, 4);
+                GetGrade(lab5, 5);
+
+                //ask if user has another student to enter (loop control)
+                Console.Write("\n\nDo you have another student to enter [y/n]: ");
+                //store user input to variable in lower for loop condition
+                strAnswer = Console.ReadLine().ToLower();
+                
+
+            } while (strAnswer == "y");
+
+
+            //determine the average for each student using a for loop based on the number of entries in the list ( list.Count())
+            for (int i = 0; i < lab1.Count(); i++ )
+            {
+                dblAvg = (lab1[i] + lab2[i] + lab3[i] + lab4[i] + lab5[i]) / 5;
+                studentAvg.Add(dblAvg);
+                
+            }
+
+            //determine the letter grade based on the average using a for loop and add it to letter grade list
+            for (int i = 0; i < studentAvg.Count(); i++)
+
+                if (studentAvg[i] >= 90)
+                {
+                    ltrGrade.Add("A");
+                }
+
+                else if (studentAvg[i] >= 80)
+                {
+                    ltrGrade.Add("B");
+                }
+
+                else if (studentAvg[i] >= 70)
+                {
+                    ltrGrade.Add("C");
+                }
+
+                else if (studentAvg[i] >= 60)
+                {
+                    ltrGrade.Add("D");
+                }
+
+                else
+                {
+                    ltrGrade.Add("F");
+                }
+
+
+
+            //display the information for each student's data using a for loop
+            for (int i = 0; i < fName.Count(); i++)
+            {
+                Console.WriteLine($"{fName[i]} {lName[i]}");
+                Console.WriteLine($"Lab 1: {lab1[i]} Lab 2: {lab2[i]} Lab 3: {lab3[i]} Lab 4: {lab4[i]} Lab 5: {lab5[i]}");
+                Console.WriteLine($"Student Average: {studentAvg[i]} Letter Grade: {ltrGrade[i]}\n\n");
+            }
+
+            //calculate the total of all grades from each lab to be used for averages
+            for (int i = 0; i < lab1.Count(); i++)
+            {
+                dblLab1Total += lab1[i];
+                dblLab2Total += lab2[i];
+                dblLab3Total += lab3[i];
+                dblLab4Total += lab4[i];
+                dblLab5Total += lab5[i];
+            }
+
+            //display the averages for each lab rounded to the 2nd decimal
+            Console.WriteLine("\nAverages for Each Lab");
+            Console.WriteLine($"Lab 1: {Math.Round(dblLab1Total / lab1.Count(), 2)} Lab 2: {Math.Round(dblLab2Total / lab2.Count(), 2)} Lab 3: {Math.Round(dblLab3Total / lab3.Count(), 2)} Lab 4: {Math.Round(dblLab4Total / lab4.Count(), 2)} Lab 5: {Math.Round(dblLab5Total / lab5.Count(),2 )}");
+
+            //Pause the console so that the user can view the information prior to the application closing
+            Console.WriteLine("\n\nPress any key to exit . . .");
+            Console.ReadKey();
+        }
+    }
+}
