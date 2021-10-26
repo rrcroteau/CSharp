7de3e7891a099834f39779b9e9d15758cdd934ad
Croteau <rrcroteau@email.neit.edu>
2021-10-26 09:25:11 -0400

    W4D1

---
+++ Week4/Lab4_Croteau/BasicTools.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_Croteau
+{
+    public class BasicTools
+    {
+        /// <summary>
+        /// Causes the program to pause until user hits Enter key
+        /// </summary>
+
+        public static void Pause()
+        {
+        
+            //print a prompt
+            Console.WriteLine("\n\nPress the Enter key to continue");
+
+            //use input to cause system to wait for Enter key to be pressed
+            Console.Read();
+
+        }
+            
+    }
+}
---
+++ Week4/Lab4_Croteau/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_Croteau
+{
+    class Program
+    {
+        //create a structure to hold a person's information
+        struct Person
+        {
+            public string FName; //first name
+            public string MName; //middle name
+            public string LName; //last name
+            public string Street1; 
+            public string Street2;
+            public string City;
+            public string State;
+            public string Zip;
+            public string Phone;
+            public string Email;
+        }
+
+        static void Main(string[] args)
+        {
+
+            //create a person
+            Person temp = new Person();
+
+            //get data to fill person
+            Console.Write("Please enter the person's first name: ");
+            temp.FName = Console.ReadLine();
+
+            Console.Write("Please enter the person's middle name: ");
+            temp.MName = Console.ReadLine();
+
+            Console.Write("Please enter the person's last name: ");
+            temp.LName = Console.ReadLine();
+
+
+
+            //output person data
+            Console.WriteLine($"New Member: {temp.FName} {temp.MName} {temp.LName}");
+
+            //let user decide when to end the program (pause the output before closing)
+            BasicTools.Pause();
+        }
+    }
+}
