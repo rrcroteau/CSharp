2b1ec78c560263bcfeceeda1930e02789a2cec82
Croteau <rrcroteau@email.neit.edu>
2022-02-16 09:06:46 -0500

    Week 6 Activity Complete

---
+++ SE256_NET/SE256_RazorActivity_RonC/TroubleTickets/Models/TroubleTicketModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TroubleTickets.Models
+{
+    public class TroubleTicketModel
+    {
+        public int Ticket_ID { get; set; } //Primary Key, Identity Spec
+
+        public string Ticket_Title { get; set; } //basic description (255 chars)
+
+        public string Ticket_Desc { get; set; } //more descriptive than the title, text field
+
+        public string Category { get; set; } //category the problem falls into 
+
+        public string Reporting_Email { get; set; } //email of person reporting the problem
+
+        public DateTime Orig_Date { get; set; } //date ticket was posted
+
+        public DateTime Close_Date { get; set; } //date ticket is considered closed
+
+        public string Responder_Notes { get; set; } //notes from the responding tech support (text)
+
+        public string Responder_Email { get; set; } //email of the responding tech support
+
+        public bool Active { get; set; } //ticket is open (true) or closed (false) - Binary field
+    }
+}
---
+++ SE256_NET/SE256_RazorActivity_RonC/TroubleTickets/Pages/AddTicket.cshtml.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TroubleTickets.Models; //use in order create trouble ticket object
+
+namespace TroubleTickets.Pages
+{
+    public class AddTicketModel : PageModel
+    {
+        [BindProperty] //requires obj to come from a form
+        public TroubleTicketModel tTicket { get; set; } //create instance of ticket model
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            IActionResult temp; //temporary result variable
+
+            if (ModelState.IsValid == false) //if there are errors, point to this page
+            {
+                temp = Page();
+            }
+
+            else
+            {
+                temp = Page();
+            }
+
+            return temp;
+        }
+    }
+}
