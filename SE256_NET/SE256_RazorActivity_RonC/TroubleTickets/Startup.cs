0d5a35c240f3280c9f557061487aef7953ad275a
Croteau <rrcroteau@email.neit.edu>
2022-03-07 09:16:28 -0500

    Week 9 Activity Start

--- SE256_NET/SE256_RazorActivity_RonC/TroubleTickets/Startup.cs
+++ SE256_NET/SE256_RazorActivity_RonC/TroubleTickets/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Session; //used to access Session Vars
 
 namespace TroubleTickets
 {
